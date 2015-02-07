a80c69181191307c721bd83fffbc21b3f7cf5a23
ashwini.rai <ashwini.rai@nsn.com>
2015-02-08 03:21:37 +0530

    adding plot1.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+plot1 <- function()
+{
+  rawdata <- read.table("household_power_consumption.txt",header=T,sep=";",na.strings="?",colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"));
+  data <- rawdata[(rawdata$Date=="1/2/2007" | rawdata$Date=="2/2/2007"),]
+  print(dim(data))
+  hist(data$Global_active_power,col="red",xlab="Global Active Power(kilowatts)",main="Global Active Power")
+  dev.copy(png,file="plot1.png")
+  dev.off()
+
+}
